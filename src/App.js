b64cf1e29334f2e2d1c6b9ed2a8497db6edcf3ff
vishnuvijayakumar25vv@gmail.com <vishn@VISHNU_V>
2023-06-05 18:33:09 +0530

    push all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef,useState } from 'react';
+import emailjs from '@emailjs/browser';
+import classes from './App.module.css';
 
 function App() {
+  const form = useRef()
+  const [isSend, setIsSend] = useState(false)
+
+  const handleSendEmail = async (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_wqdr677", "template_wja7vnt", form.current, "9EpTUCppV0f1KBhnQ")
+      .then((result) => {
+        console.log(result.text);
+        setIsSend(true)
+        setTimeout(() => {
+              setIsSend(false)
+        },2500)
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
+
+  const handleClick = (e) => {
+  href="https://vishnusidea.github.io/VISHNU-website/";
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className={classes.container}>
+      <form ref={form} onSubmit={handleSendEmail}>
+        <div className={classes.inputBox}>
+          <label>Name</label>
+          <input type="text" name="from_name" placeholder='Username' />
+        </div>
+        <div className={classes.inputBox}>
+          <label>Genter</label>
+          <input type="text" name="gender"  placeholder='Male/Female'/>
+        </div>
+
+        <div className={classes.inputBox}>
+          <label>Email</label>
+          <input type="email" name="email" placeholder='Enteryouremail' />
+        </div>
+
+        <div className={classes.inputBox}>
+          <label>Message</label>
+          <textarea name="message" cols="30" rows="10" placeholder='Review for this webpage.....' />
+        </div>
+        <button type='submit' onClick={handleClick}>Send</button>
+        {isSend && (
+          <div className={classes.isSendmessage}>
+            Email has been sent successfully
+          </div>
+        )}
+      </form>
     </div>
   );
 }
